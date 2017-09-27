1c072795ed19a59c04b5205c756987ecd16348c5
chenghong <chenghong@rainbow.lan>
2017-09-27 15:37:45 +0800

    gengxin

--- src/main/java/cn/eureka/eurekaserver/Test.java
+++ src/main/java/cn/eureka/eurekaserver/Test.java
@@ -1,5 +1,8 @@
 package cn.eureka.eurekaserver;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.util.StringUtils;
 
 import com.google.common.base.Strings;
@@ -8,6 +11,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Map<String, Object> user=new HashMap<String,Object>();
+		user.put("type", 4);
+		System.out.println(user.get("type").toString().equals("4"));
+		
 		String str="http://www.baidu.com/,http://127.0.0.1:2222/rainbow/";
 		Strings.isNullOrEmpty(str);
 		if(!StringUtils.isEmpty(str)){
